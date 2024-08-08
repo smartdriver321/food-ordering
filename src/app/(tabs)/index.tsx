cddad166510e714617e7cba04f983690d688ccc1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-08 15:42:56 +0700

    Implement typescript path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,6 @@
+import ProductListItem from '@/components/ProductListItem'
+import products from '@assets/data/products'
 import { View } from 'react-native'
-
-import products from '@/assets/data/products'
-import ProductListItem from '@/src/components/ProductListItem'
 
 export default function MenuScreen() {
 	return (
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,22 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 
+import { Product } from '../types'
 import Colors from '../constants/Colors'
 
-export default function ProductListItem({ product }) {
+export const defaultPizzaImage =
+	'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+type ProductListItemProps = {
+	product: Product
+}
+
+export default function ProductListItem({ product }: ProductListItemProps) {
 	return (
 		<View style={styles.container}>
-			<Image source={{ uri: product.image }} style={styles.image} />
+			<Image
+				source={{ uri: product.image || defaultPizzaImage }}
+				style={styles.image}
+			/>
 
 			<Text style={styles.title}>{product.name}</Text>
 			<Text style={styles.price}>${product.price}</Text>
