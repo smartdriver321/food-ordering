f622d32111827c58cdeb44222a20c764a3644bef
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-11 15:56:41 +0700

    Add user authentication

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native'
 import { Link, Stack } from 'expo-router'
 
+import { supabase } from '@/lib/supabase'
 import Button from '@/components/Button'
 import Colors from '@/constants/Colors'
 
 export default function SignInScreen() {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	async function signInWithEmail() {
+		setLoading(true)
+		const { error } = await supabase.auth.signInWithPassword({
+			email,
+			password,
+		})
+
+		if (error) Alert.alert(error.message)
+		setLoading(false)
+	}
 
 	return (
 		<View style={styles.container}>
@@ -30,7 +43,11 @@ export default function SignInScreen() {
 				secureTextEntry
 			/>
 
-			<Button text='Sign in' />
+			<Button
+				onPress={signInWithEmail}
+				disabled={loading}
+				text={loading ? 'Signing in...' : 'Sign in'}
+			/>
 			<Link href='/sign-up' style={styles.textButton}>
 				Create an account
 			</Link>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, StyleSheet, Alert } from 'react-native'
 import { Link, Stack } from 'expo-router'
 
+import { supabase } from '@/lib/supabase'
 import Button from '@/components/Button'
 import Colors from '@/constants/Colors'
 
 export default function SignUpScreen() {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
+	const [loading, setLoading] = useState(false)
+
+	async function signUpWithEmail() {
+		setLoading(true)
+
+		const { error } = await supabase.auth.signUp({ email, password })
+
+		if (error) Alert.alert(error.message)
+		setLoading(false)
+	}
 
 	return (
 		<View style={styles.container}>
@@ -30,7 +41,12 @@ export default function SignUpScreen() {
 				secureTextEntry
 			/>
 
-			<Button text='Create account' />
+			<Button
+				onPress={signUpWithEmail}
+				disabled={loading}
+				text={loading ? 'Creating account...' : 'Create account'}
+			/>
+
 			<Link href='/sign-in' style={styles.textButton}>
 				Sign in
 			</Link>
